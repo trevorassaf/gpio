553041eec30fc0ee40754e6a3ea285e6a06e1c6f
Trevor Assaf <trevor.assaf@gmail.com>
2015-08-07 13:54:04 -0400

    pin header

--- cpp/pin/pin.h
+++ cpp/pin/pin.h
@@ -2,6 +2,8 @@
 
 #include <pin/pin_manager.h>
 
+#include <cstdint>
+
 namespace Gpio {
   class Pin {
     private:
@@ -11,12 +13,16 @@ namespace Gpio {
       }; 
 
       PinFunction _currentPinFunction;
+
+      const uint8_t _pinId;
      
       friend class Gpio::PinManager;
-      Gpio();
+      Pin(const uint8_t pin_id);
+      ~Pin();
 
     public:
-      ~Gpio();
+      Pin(const Pin & pin) = delete;
+      Pin & operation=(const Pin & pin) = delete;
       bool read(); 
       void write(bool value);
       void clear();
